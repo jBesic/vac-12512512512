7dd1d1ba1de430bc76b45e7da264f7f4ee806a79
Jasmin Besic <jasmin.besic@hhinc.eu>
2018-02-09 09:21:27 +0100

    Canvas improvements

--- src/components/Toolbox/Toolbox.js
+++ src/components/Toolbox/Toolbox.js
@@ -10,7 +10,6 @@ import line from '../../assets/images/line.svg';
 import text from '../../assets/images/text.svg';
 
 import { tools } from '../../helper/canvasHelper';
-import ShapeProperties from '../ShapeProperties';
 
 import './Toolbox.css';
 
@@ -27,42 +26,42 @@ const Toolbox = (props) => {
             <div className='row toolbar-tools'>
                 <div className='col-md-6'>
                     <div className={'toolbox__tool' + (props.selectedTool === tools.SELECT ? ' toolbox__tool-selected' : '') + (props.drawStarted ? ' toolbox__tool-disabled' : '')} onClick={() => props.selectToolHandler(tools.SELECT)}>
-                        <img className="img-fluid" src={select} />
+                        <img alt='Toolbar icon' className="img-fluid" src={select} />
                     </div>
                 </div>
                 <div className='col-md-6'>
                     <div className={'toolbox__tool' + (props.selectedTool === tools.DELETE ? ' toolbox__tool-selected' : '') + (props.drawStarted ? ' toolbox__tool-disabled' : '')} onClick={() => props.selectToolHandler(tools.DELETE)}>
-                        <img className="img-fluid" src={eraser} />
+                        <img alt='Toolbar icon' className="img-fluid" src={eraser} />
                     </div>
                 </div>
                 <div className='col-md-6'>
                     <div className={'toolbox__tool' + (props.selectedTool === tools.RECTANGLE ? ' toolbox__tool-selected' : '') + (props.drawStarted ? ' toolbox__tool-disabled' : '')} onClick={() => props.selectToolHandler(tools.RECTANGLE)}>
-                        <img className="img-fluid" src={rectangle} />
+                        <img alt='Toolbar icon' className="img-fluid" src={rectangle} />
                     </div>
                 </div>
                 <div className='col-md-6'>
                     <div className={'toolbox__tool' + (props.selectedTool === tools.TRIANGLE ? ' toolbox__tool-selected' : '') + (props.drawStarted ? ' toolbox__tool-disabled' : '')} onClick={() => props.selectToolHandler(tools.TRIANGLE)}>
-                        <img className="img-fluid" src={triangle} />
+                        <img alt='Toolbar icon' className="img-fluid" src={triangle} />
                     </div>
                 </div>
                 <div className='col-md-6'>
                     <div className={'toolbox__tool' + (props.selectedTool === tools.CIRCLE ? ' toolbox__tool-selected' : '') + (props.drawStarted ? ' toolbox__tool-disabled' : '')} onClick={() => props.selectToolHandler(tools.CIRCLE)}>
-                        <img className="img-fluid" src={circle} />
+                        <img alt='Toolbar icon' className="img-fluid" src={circle} />
                     </div>
                 </div>
                 <div className='col-md-6'>
                     <div className={'toolbox__tool' + (props.selectedTool === tools.LINE ? ' toolbox__tool-selected' : '') + (props.drawStarted ? ' toolbox__tool-disabled' : '')} onClick={() => props.selectToolHandler(tools.LINE)}>
-                        <img className="img-fluid" src={line} />
+                        <img alt='Toolbar icon' className="img-fluid" src={line} />
                     </div>
                 </div>
                 <div className='col-md-6'>
                     <div className={'toolbox__tool' + (props.selectedTool === tools.POLYGON ? ' toolbox__tool-selected' : '') + (props.drawStarted ? ' toolbox__tool-disabled' : '')} onClick={() => props.selectToolHandler(tools.POLYGON)}>
-                        <img className="img-fluid" src={polygon} />
+                        <img alt='Toolbar icon' className="img-fluid" src={polygon} />
                     </div>
                 </div>
                 <div className='col-md-6'>
                     <div className={'toolbox__tool' + (props.selectedTool === tools.TEXT_INPUT ? ' toolbox__tool-selected' : '') + (props.drawStarted ? ' toolbox__tool-disabled' : '')} onClick={() => props.selectToolHandler(tools.TEXT_INPUT)}>
-                        <img className="img-fluid" src={text} />
+                        <img alt='Toolbar icon' className="img-fluid" src={text} />
                     </div>
                 </div>
             </div>
